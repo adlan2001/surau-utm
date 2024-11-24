df6ec206228f8697949722241cb766bffd47f511
Adlan Uzair bin Amer Hafizi <118761084+adlan2001@users.noreply.github.com>
2024-11-24 12:40:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var map = L.map('map').setView([1.564, 103.638], 15);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | \
-  <a href="https://www.flaticon.com/free-icons/mosque" title="mosque icons">Mosque icons by nawicon</a> | adlan2001'
+  <a href="https://www.flaticon.com/free-icons/mosque" title="mosque icons">Mosque icons by nawicon</a> | <a href="https://github.com/adlan2001" title="adlan2001 Github">adlan2001</a>'
 }).addTo(map);
 
 const surauIcon = L.icon({
